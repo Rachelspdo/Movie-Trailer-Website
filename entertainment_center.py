21596ddbda06d93cd4faa9d427cf1252f4b88269
Rachel Do <rachelspdo20@gmail.com>
2017-10-03 22:16:37 -0700

    Add comments to each movie Instances

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,7 @@
 import media  # import media that we create Class
 import fresh_tomatoes
+
+# Wonder Women: movie title, storyline, poster image and movie trailer
 wonder_women = media.Movie(
                         "Wonder Women",
                         "Before she was Wonder Woman, she was Diana...",
@@ -7,6 +9,7 @@ wonder_women = media.Movie(
                         "https://www.youtube.com/watch?v=VSB4wGIdDwo"
                         )
 
+# Independent Day: movie title, storyline, poster image and movie trailer
 independent_day = media.Movie(
                             "Independent Day",
                             "The aliens are coming and invading Earth...",
@@ -14,6 +17,7 @@ independent_day = media.Movie(
                             "https://www.youtube.com/watch?v=O3a0kv1sJxg"
                             )
 
+# Hidden Figures: movie title, storyline, poster image and movie trailer
 hidden_figures = media.Movie(
                             "Hidden Figures",
                             "Three brilliant African-American women at NASA..",
@@ -21,6 +25,7 @@ hidden_figures = media.Movie(
                             "https://www.youtube.com/watch?v=q8FZCYtyxQk"
                             )
 
+# Spider-Man: Homecoming: movie title, storyline, poster image, movie trailer
 spider_man = media.Movie(
                         "Spider-Man: Homecoming",
                         "Peter Parker tries to balance his life with his...",
@@ -28,6 +33,7 @@ spider_man = media.Movie(
                         "https://www.youtube.com/watch?v=U0D3AOldjMU"
                         )
 
+# 47 Meters Down: movie title, storyline, poster image and movie trailer
 meters_down = media.Movie(
                         "47 Meters Down",
                         "Kate and Lisa go diving in shark-infested waters...",
@@ -35,6 +41,7 @@ meters_down = media.Movie(
                         "https://www.youtube.com/watch?v=iKjvse77vjU"
                         )
 
+# The Lost City of Z: movie title, storyline, poster image and movie trailer
 lost_city = media.Movie(
                         "The Lost City of Z",
                         "British explorer Percy Fawcett journeys into the...",
@@ -42,8 +49,10 @@ lost_city = media.Movie(
                         "https://www.youtube.com/watch?v=S0eY-7uYcCU"
                         )
 
-# wonder_women.show_trailer()
+# List the movies that will be passed in media file
 movies = [
     wonder_women, independent_day, hidden_figures,
     spider_man, meters_down, lost_city]
+
+# Open the webbroser through fresh_tomatoes.py
 fresh_tomatoes.open_movies_page(movies)
